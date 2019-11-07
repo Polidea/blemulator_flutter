25b8175953a33de2cf621f84aed0b52126a72520
pawelByszewski <pawell.b@gmail.com>
2019-11-07 07:54:05 +0100

    refactor

--- example/test_driver/instrumentation/test_isolate/test_devices.dart
+++ example/test_driver/instrumentation/test_isolate/test_devices.dart
@@ -1,9 +1,20 @@
 
 
+import 'dart:convert';
+
+import 'package:flutter_driver/flutter_driver.dart';
+
+import '../Command.dart';
+
 class TestDevice {
   String deviceId;
 
   TestDevice(this.deviceId);
+
+  disconnect(FlutterDriver driver) {
+    var command = DeviceCommand(CommandType.DISCONNECT, deviceId);
+    driver.requestData(jsonEncode(command));
+  }
 }
 
 TestDevice unconnectablePeripheralStub = TestDevice("1");
