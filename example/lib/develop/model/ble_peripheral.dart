5d8b39e3c6f1479d6d06726d309081b99dc3c71b
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-11-14 13:24:56 +0100

    Skeleton implementation of BlePeripheral

---
+++ example/lib/develop/model/ble_peripheral.dart
@@ -0,0 +1,6 @@
+class BlePeripheral {
+  String name;
+  String id;
+  int rssi;
+  bool isConnected;
+}
--- example/lib/develop/peripheral_list/peripheral_list_state.dart
+++ example/lib/develop/peripheral_list/peripheral_list_state.dart
@@ -1,13 +1,8 @@
+import 'package:blemulator_example/develop/model/ble_peripheral.dart';
+
 class PeripheralListState {
-  final List<PeripheralInfo> peripherals;
+  final List<BlePeripheral> peripherals;
   final bool isScanningEnabled;
 
   const PeripheralListState(this.peripherals, this.isScanningEnabled);
-}
-
-class PeripheralInfo {
-  String name;
-  String id;
-  int rssi;
-  bool isConnected;
 }
