03de1c80f64182eeefa0a16f95673db26777aa2b
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-11-22 20:40:48 +0100

    Add category property to BlePeripheral

--- example/lib/model/ble_peripheral.dart
+++ example/lib/model/ble_peripheral.dart
@@ -6,8 +6,20 @@ class BlePeripheral extends Equatable {
   final int rssi;
   final bool isConnected;
 
+  BlePeripheralCategory get category => _nameToCategory(name);
+
   BlePeripheral(this.name, this.id, this.rssi, this.isConnected);
+
+  BlePeripheralCategory _nameToCategory(String name) {
+    if (name == "SensorTag") {
+      return BlePeripheralCategory.sensorTag;
+    } else {
+      return BlePeripheralCategory.other;
+    }
+  }
 
   @override
   List<Object> get props => [name, id];
 }
+
+enum BlePeripheralCategory { sensorTag, other }
