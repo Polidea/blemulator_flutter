e6b39d15d4b0d7df96de75b9930ddc810c052744
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-11-26 11:11:05 +0100

    Refactor how peripherals category is determined

--- example/lib/model/ble_peripheral.dart
+++ example/lib/model/ble_peripheral.dart
@@ -6,20 +6,23 @@ class BlePeripheral extends Equatable {
   final int rssi;
   final bool isConnected;
 
-  BlePeripheralCategory get category => _nameToCategory(name);
+  BlePeripheralCategory get category {
+    return KnownBlePeripheralCategory.isSensorTag(name) ? BlePeripheralCategory
+        .sensorTag : BlePeripheralCategory.other;
+  }
 
   BlePeripheral(this.name, this.id, this.rssi, this.isConnected);
-
-  BlePeripheralCategory _nameToCategory(String name) {
-    if (name == "SensorTag") {
-      return BlePeripheralCategory.sensorTag;
-    } else {
-      return BlePeripheralCategory.other;
-    }
-  }
 
   @override
   List<Object> get props => [name, id];
 }
 
 enum BlePeripheralCategory { sensorTag, other }
+
+class KnownBlePeripheralCategory {
+  static final String sensorTag = 'SensorTag';
+
+  static bool isSensorTag(String blePeripheralName) {
+    return blePeripheralName == sensorTag;
+  }
+}
