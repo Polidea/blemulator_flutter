8f55fc212198dc25bbb2c424c4f6969594effa83
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-12-02 15:49:44 +0100

    Remove unused import

--- example/lib/peripheral_list/peripheral_list_screen.dart
+++ example/lib/peripheral_list/peripheral_list_screen.dart
@@ -1,12 +1,10 @@
 import 'package:blemulator_example/peripheral_list/bloc.dart';
 import 'package:blemulator_example/peripheral_list/components/peripheral_row_view.dart';
-import 'package:blemulator_example/styles/custom_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PeripheralListScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final peripheralListBloc = BlocProvider.of<PeripheralListBloc>(context);
