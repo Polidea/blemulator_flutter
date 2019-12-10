a2f4ab8ffe2fe331f8bf560957a6ee0bc00059c6
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-12-10 11:31:19 +0100

    [example] Peripheral list screen design modifications (#75)
    * Apply new design to the peripheral list screen
    
    * Move property_row.dart to a common directory
    
    * Revert rebase changes
    
    * Restore padding after rebase
    
    * Improve list row accessories
    
    * Support peripheral category on the list screen
    
    * Remove unused cardTitleAccessory TextStyle
    
    * Rename PeripheralRowView to PeripheralItem
    
    * Move title and value accessory creating code to the main build method
    
    * Rename navigatorBloc to navigationBloc
    
    * Apply review suggestions
    
    * Extract rssi value formatting to a separate method

--- example/lib/peripheral_details/components/peripheral_details_view.dart
+++ example/lib/peripheral_details/components/peripheral_details_view.dart
@@ -1,5 +1,5 @@
 import 'package:blemulator_example/peripheral_details/bloc.dart';
-import 'package:blemulator_example/peripheral_details/components/property_row.dart';
+import 'package:blemulator_example/common/components/property_row.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
---
+++ example/lib/peripheral_list/components/peripheral_item.dart
@@ -0,0 +1,82 @@
+import 'package:blemulator_example/model/ble_peripheral.dart';
+import 'package:blemulator_example/navigation/bloc.dart';
+import 'package:blemulator_example/common/components/property_row.dart';
+import 'package:blemulator_example/styles/custom_text_style.dart';
+import 'package:blemulator_example/util/signal_level.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class PeripheralItem extends StatelessWidget {
+  final BlePeripheral _peripheral;
+
+  PeripheralItem(this._peripheral);
+
+  @override
+  Widget build(BuildContext context) {
+    final navigationBloc = BlocProvider.of<NavigationBloc>(context);
+
+    return PropertyRow(
+      title: _categoryDisplayName(_peripheral.category),
+      titleIcon: Icons.bluetooth,
+      titleColor: Theme.of(context).primaryColor,
+      value: _peripheral.name,
+      titleAccessory: Icon(
+        Icons.chevron_right,
+        color: Colors.grey,
+      ),
+      valueAccessory: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Text(
+            _formatRssi(_peripheral.rssi),
+            style: CustomTextStyle.cardValueAccessory
+                .copyWith(color: _colorForRssi(_peripheral.rssi)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0),
+            child: Icon(
+              Icons.settings_input_antenna,
+              color: _colorForRssi(_peripheral.rssi),
+            ),
+          ),
+        ],
+        mainAxisSize: MainAxisSize.min,
+      ),
+      onTap: () => _onRowTap(navigationBloc),
+    );
+  }
+
+  void _onRowTap(NavigationBloc navigationBloc) {
+    navigationBloc.add(NavigateToPeripheralDetails(peripheral: _peripheral));
+  }
+  
+  String _formatRssi(int rssi) {
+    return '${rssi ?? '-'} dbm';
+  }
+
+  Color _colorForRssi(int rssi) {
+    if (rssi == null) return Colors.grey;
+
+    switch (parseRssi(rssi)) {
+      case SignalLevel.high:
+        return Colors.green;
+      case SignalLevel.medium:
+        return Colors.orange;
+      case SignalLevel.low:
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  String _categoryDisplayName(BlePeripheralCategory category) {
+    switch (category) {
+      case BlePeripheralCategory.sensorTag:
+        return 'SensorTag';
+      case BlePeripheralCategory.other:
+        return 'Other';
+      default:
+        return 'Unknown';
+    }
+  }
+}
