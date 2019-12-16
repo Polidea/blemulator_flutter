5715461cf4df09cb1c50e00e6366280c8509e047
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-12-16 16:25:04 +0100

    Store titleIcon size in CustomSizes class

--- example/lib/common/components/title_icon.dart
+++ example/lib/common/components/title_icon.dart
@@ -1,6 +1,7 @@
+import 'package:blemulator_example/styles/custom_sizes.dart';
 import 'package:flutter/widgets.dart';
 
 class TitleIcon extends Icon {
   TitleIcon(IconData icon, {Color color})
-      : super(icon, size: 20.0, color: color);
+      : super(icon, size: CustomSizes.titleIcon, color: color);
 }
--- example/lib/common/components/title_image_icon.dart
+++ example/lib/common/components/title_image_icon.dart
@@ -1,6 +1,7 @@
+import 'package:blemulator_example/styles/custom_sizes.dart';
 import 'package:flutter/widgets.dart';
 
 class TitleImageIcon extends ImageIcon {
   TitleImageIcon(ImageProvider image, {Color color})
-      : super(image, size: 20.0, color: color);
+      : super(image, size: CustomSizes.titleIcon, color: color);
 }
