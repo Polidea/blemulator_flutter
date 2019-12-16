e4eb08bed1ba9972147ed2bef04cb0dcd45b870c
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-12-16 16:07:56 +0100

    Add TitleImageIcon class

---
+++ example/lib/common/components/title_image_icon.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+class TitleImageIcon extends ImageIcon {
+  TitleImageIcon(ImageProvider image, {Color color})
+      : super(image, size: 20.0, color: color);
+}
