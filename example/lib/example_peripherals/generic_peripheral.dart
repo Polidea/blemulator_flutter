f29e7b86cc29ccf4cab5824543b32f675a2664ac
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-12-18 12:31:51 +0100

    Enhance GenericPeripheral

--- example/lib/example_peripherals/generic_peripheral.dart
+++ example/lib/example_peripherals/generic_peripheral.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:blemulator/blemulator.dart';
 
 class GenericPeripheral extends SimulatedPeripheral {
@@ -7,8 +9,32 @@ class GenericPeripheral extends SimulatedPeripheral {
       int milliseconds = 1000,
       List<SimulatedService> services = const []})
       : super(
-            name: name,
-            id: id,
-            advertisementInterval: Duration(milliseconds: milliseconds),
-            services: services);
+          name: name,
+          id: id,
+          advertisementInterval: Duration(milliseconds: milliseconds),
+          services: [
+            SimulatedService(
+                uuid: 'F000AA00-0001-4000-B000-000000000000',
+                isAdvertised: true,
+                characteristics: [
+                  SimulatedCharacteristic(
+                      uuid: 'F000AA10-0001-4000-B000-000000000000',
+                      value: Uint8List.fromList([0]),
+                      convenienceName: 'Generic characteristic'),
+                ],
+                convenienceName: 'Generic service'),
+          ],
+        );
+
+  @override
+  Future<bool> onConnectRequest() async {
+    await Future.delayed(Duration(milliseconds: 500));
+    return super.onConnectRequest();
+  }
+
+  @override
+  Future<void> onDiscoveryRequest() async {
+    await Future.delayed(Duration(milliseconds: 1000));
+    return super.onDiscoveryRequest();
+  }
 }
