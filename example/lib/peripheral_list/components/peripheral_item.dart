6eaeef5f372a3d0e749f416da83d925b18a81704
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-12-18 19:27:21 +0100

    Extract peripheral category icon to a separate widget

--- example/lib/peripheral_list/components/peripheral_item.dart
+++ example/lib/peripheral_list/components/peripheral_item.dart
@@ -1,10 +1,8 @@
-import 'package:blemulator_example/common/components/title_icon.dart';
-import 'package:blemulator_example/common/components/title_image_icon.dart';
 import 'package:blemulator_example/model/ble_peripheral.dart';
 import 'package:blemulator_example/navigation/bloc.dart';
 import 'package:blemulator_example/common/components/property_row.dart';
+import 'package:blemulator_example/peripheral_list/components/peripheral_category_icon.dart';
 import 'package:blemulator_example/peripheral_list/components/rssi_view.dart';
-import 'package:blemulator_example/styles/custom_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,7 +17,7 @@ class PeripheralItem extends StatelessWidget {
 
     return PropertyRow(
       title: _peripheral.id,
-      titleIcon: _iconForPeripheral(context),
+      titleIcon: PeripheralCategoryIcon(_peripheral.category),
       titleColor: _peripheral.category.color(context),
       value: _peripheral.name,
       titleAccessory: Icon(
@@ -33,19 +31,5 @@ class PeripheralItem extends StatelessWidget {
 
   void _onRowTap(NavigationBloc navigationBloc) {
     navigationBloc.add(NavigateToPeripheralDetails(peripheral: _peripheral));
-  }
-
-  Widget _iconForPeripheral(BuildContext context) {
-    if (_peripheral.category == BlePeripheralCategory.sensorTag) {
-      return TitleImageIcon(
-        AssetImage('assets/ti_logo.png'),
-        color: CustomColors.sensorTagRed,
-      );
-    } else {
-      return TitleIcon(
-        Icons.bluetooth,
-        color: Theme.of(context).primaryColor,
-      );
-    }
   }
 }
