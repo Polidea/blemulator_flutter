a843a875bbadcae3444c9b2eed6404e010845153
Tomasz Bogusz <tomasz.bogusz@polidea.com>
2019-12-18 19:26:05 +0100

    Restore signal_level.dart file in an enhanced way

---
+++ example/lib/util/signal_level.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+enum SignalLevel { high, medium, low, unknown }
+
+extension SignalLevelExtenstion on SignalLevel {
+  Color color() {
+    switch (this) {
+      case SignalLevel.high:
+        return Colors.green;
+      case SignalLevel.medium:
+        return Colors.orange;
+      case SignalLevel.low:
+        return Colors.red;
+      case SignalLevel.unknown:
+        return Colors.grey;
+      default:
+        return Colors.black;
+    }
+  }
+}
+
+SignalLevel signalLevelForRssi(int rssi) {
+  if (rssi == null) return SignalLevel.unknown;
+  if (rssi > -60) {
+    return SignalLevel.high;
+  } else if (rssi > -90) {
+    return SignalLevel.medium;
+  } else {
+    return SignalLevel.low;
+  }
+}
